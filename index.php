1e22639ceae2128b847373ff35d7660307347c74
simonematsukis-22202308 <85491537+simonematsukis-22202308@users.noreply.github.com>
2021-06-07 13:20:54 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Simone Matsukis - Honours Research Project</title>
+
+    <!-- Master Stylesheet CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300&display=swap" rel="stylesheet">
+
+    <!--Icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div id="preloader-area">
+        <div class="lds-ripple">
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+    <!-- Preloader -->
+
+    <!-- Main Page -->
+    <div class="main-panel">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-xl">
+                    <!-- Card -->
+
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col">
+                            <div class='text-center'>
+                                <!-- Title -->
+                                <h1 class="align-text-center"><strong>Thank you for participating in my honours research project!</strong> </h1>
+
+                                <!-- Heading -->
+                                <span class="font-18 text-white mb-0">
+                                            I aim to develop a machine learning algorithm that automatically classifies a student into their relevant
+                                            learning style based on their interaction with online learning systems. The data you supply will be used
+                                            to train my algorithms.<br><br>
+                                            There is 2 parts to this study. The first part will require personal data from you, and the second part
+                                            will require you to answer a few quiz-like questions as you would on your institute's learning platform (Blackboard, Moodle etc.).
+                                            <br><br>
+                                            <strong>Total time required</strong>  ~ 15-20 minutes.
+                                            <br><br>
+                                            While answering the questions, your interaction with the site will be monitored. You will remain anonymous. If you would like
+                                            to view more information on my research project, you can do so <a href='#'>here</a>. Thank you in advance for taking the time out of your
+                                            day to assist me with my research project, it is greatly appreciated!
+                                        </span>
+                                <br><br>
+
+                                <div>
+                                <button type="button" class="btn btn-lg btn-rounded btn-yellow mb-2 mr-2" onclick="location.href = 'personal-details.php';">LET'S BEGIN! <i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+                                </div>
+                            </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            <!-- / .row -->
+
+
+
+
+        </div>
+
+    </div>
+
+    <!-- Plugins Js -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bundle.js"></script>
+
+    <!-- Active JS -->
+    <script src="js/settings.js"></script>
+    <script src="js/default-assets/active.js"></script>
+
+    <!-- Inject JS -->
+    <script src="js/default-assets/mini-event-calendar.min.js"></script>
+    <script src="js/default-assets/mini-calendar-active.js"></script>
+    <script src="js/default-assets/apexchart.min.js"></script>
+    <script src="js/default-assets/dashboard-active.js"></script>
+
+</body>
+
+</html>
---
+++ learning-system.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Simone Matsukis - Honours Research Project</title>
+
+    <!-- Master Stylesheet CSS -->
+    <link rel="stylesheet" href="style.css">
+
+      <!-- Font -->
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300&display=swap" rel="stylesheet">
+
+    <!--Icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div id="preloader-area">
+        <div class="lds-ripple">
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+    <!-- Preloader -->
+
+    <!-- Main Page -->
+    <div class="main-panel">
+        <div class="container-fluid">
+<br>
+        <h2 class="align-text-center">Simulated Learning System</h2>
+            <br>
+
+
+            <div class="row">
+                <div class="col-12 box-margin height-card">
+                    <div class="card">
+                        <div class="card-body">
+
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <!-- Plugins Js -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bundle.js"></script>
+
+    <!-- Active JS -->
+    <script src="js/settings.js"></script>
+    <script src="js/default-assets/active.js"></script>
+
+    <!-- Inject JS -->
+    <script src="js/default-assets/mini-event-calendar.min.js"></script>
+    <script src="js/default-assets/mini-calendar-active.js"></script>
+    <script src="js/default-assets/apexchart.min.js"></script>
+    <script src="js/default-assets/dashboard-active.js"></script>
+
+</body>
+
+</html>
